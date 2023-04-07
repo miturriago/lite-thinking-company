e77aabf303e2ce5f11713438f9fc59457d2f2bae
Mauricio Maldonado <miturriago@uninorte.edu.co>
2023-04-07 13:47:00 -0500

    create a function to add or update a company

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-module.exports.handler = async (event) => {
+"use strict";
+require("dotenv").config();
+const dynamoDb = require("./services/dynamo.service");
+const ajvO = require("ajv");
+const ajvRq = new ajvO();
+
+module.exports.createCompany = async (event) => {
+  const data = JSON.parse(event.body);
+  const { fullName, nit, address, phone } = data;
+  const creationDate = new Date().toDateString();
+  const updateDate = creationDate;
+  const PK = "#COMPANIES";
+  const SK = "#COMPANY#" + nit;
+  const payload = {
+    PK,
+    SK,
+    fullName,
+    nit,
+    address,
+    phone,
+    creationDate,
+    updateDate,
+  };
+
+  try {
+    await dynamoDb.putItem(
+      payload,
+      process.env.TABLE_NAME + "-" + process.env.STAGE
+    );
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      error: JSON.stringify(error),
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({ message: "success", payload }),
   };
 };
