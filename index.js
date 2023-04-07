b1da84e9c45ad177aa0a5bb63380a996bc985987
Mauricio Maldonado <miturriago@uninorte.edu.co>
2023-04-07 13:59:30 -0500

    add validation for createCompany function

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ require("dotenv").config();
 const dynamoDb = require("./services/dynamo.service");
 const ajvO = require("ajv");
 const ajvRq = new ajvO();
+const schemaCreateCompanyRq = require("./schemas/rqCreateCompanySchema.json");
+const validateCreateRq = ajvRq.compile(schemaCreateCompanyRq);
 
 module.exports.createCompany = async (event) => {
   const data = JSON.parse(event.body);
+  let valid = validateCreateRq(data);
+
+  if (!valid) {
+    return {
+      statusCode: 406,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        message: "Empty fields are not accepted",
+        details: validateCreateRq.errors[0],
+      }),
+    };
+  }
+
   const { fullName, nit, address, phone } = data;
   const creationDate = new Date().toDateString();
   const updateDate = creationDate;
